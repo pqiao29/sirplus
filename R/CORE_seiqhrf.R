faa3aae851c39fd14e191a46f05c758e862f7ac3
pqiao29 <pqiao@student.unimelb.edu.au>
2020-05-12 16:47:01 +1000

    remove if(ncores == 1) from seiqhrf

--- R/CORE_seiqhrf.R
+++ R/CORE_seiqhrf.R
@@ -70,151 +70,89 @@ merge.seiqhrf.icm <- function(x, y, ...) {
 #' @importFrom future availableCores
 #' @export
 seiqhrf <- function(init, control, param, seed = NULL) {
-
-
+  
+  
   crosscheck_seiqhrf(param, init, control)
   nsims <- control$nsims
   ncores <- ifelse(control$nsims == 1, 1, min(future::availableCores(), control$ncores))
   control$ncores <- ncores
-
-  if (ncores == 1) {
-
-      # Simulation loop start
-      for (s in 1:control$nsims) {
-
-        ## Initialization module
-        if (!is.null(control[["initialize.FUN"]])) {
-          dat <- do.call(control[["initialize.FUN"]], list(param, init, control, seed))
-        }
-
-
-        # Timestep loop
-        for (at in 2:control$nsteps) {
-
-          ## Infection
-          if (!is.null(control[["infection.FUN"]])) {
-            dat <- do.call(control[["infection.FUN"]], list(dat, at, seed))
-          }
-
-
-          ## Recovery
-          if (!is.null(control[["recovery.FUN"]])) {
-            dat <- do.call(control[["recovery.FUN"]], list(dat, at, seed))
-          }
-
-
-          ## Departure Module
-          if (!is.null(control[["departures.FUN"]])) {
-            dat <- do.call(control[["departures.FUN"]], list(dat, at, seed))
-          }
-
-
-          ## Arrival Module
-          if (!is.null(control[["arrivals.FUN"]])) {
-            dat <- do.call(control[["arrivals.FUN"]], list(dat, at, seed))
-          }
-
-
-          ## Outputs
-          if (!is.null(control[["get_prev.FUN"]])) {
-            dat <- do.call(control[["get_prev.FUN"]], list(dat, at))
-          }
-
-
-          ## Track progress
-          EpiModel::verbose.icm(dat, type = "progress", s, at)
-        }
-
-        # Set output
-        if (s == 1) {
-          out <- saveout_seiqhrf(dat, s)
-        } else {
-          out <- saveout_seiqhrf(dat, s, out)
-        }
-
-      } # Simulation loop end
-
-  } # end of single core execution
-
-  if (ncores > 1) {
-    doParallel::registerDoParallel(ncores)
-
-    sout <- foreach(s = 1:nsims) %dopar% {
-
-        control$nsims <- 1
-        control$currsim <- s
-
-        ## Initialization module
-        if (!is.null(control[["initialize.FUN"]])) {
-          dat <- do.call(control[["initialize.FUN"]], list(param, init, control, seed))
-        }
-
-        # Timestep loop
-        for (at in 2:control$nsteps) {
-
-          ## Infection
-          if (!is.null(control[["infection.FUN"]])) {
-            dat <- do.call(control[["infection.FUN"]], list(dat, at, seed))
-          }
-
-
-          ## Recovery
-          if (!is.null(control[["recovery.FUN"]])) {
-            dat <- do.call(control[["recovery.FUN"]], list(dat, at, seed))
-          }
-
-
-          ## Departure Module
-          if (!is.null(control[["departures.FUN"]])) {
-            dat <- do.call(control[["departures.FUN"]], list(dat, at, seed))
-          }
-
-
-          ## Arrival Module
-          if (!is.null(control[["arrivals.FUN"]])) {
-            dat <- do.call(control[["arrivals.FUN"]], list(dat, at, seed))
-          }
-
-
-          ## Outputs
-          if (!is.null(control[["get_prev.FUN"]])) {
-            dat <- do.call(control[["get_prev.FUN"]], list(dat, at))
-          }
-
-
-          ## Track progress
-          EpiModel::verbose.icm(dat, type = "progress", s, at)
-        }
-
-        # Set output
-        out <- saveout_seiqhrf(dat, s=1)
-        class(out) <- "icm"
-        return(out)
-
+  
+  
+  doParallel::registerDoParallel(ncores)
+  
+  sout <- foreach(s = 1:nsims) %dopar% {
+    
+    control$nsims <- 1
+    control$currsim <- s
+    
+    ## Initialization module
+    if (!is.null(control[["initialize.FUN"]])) {
+      dat <- do.call(control[["initialize.FUN"]], list(param, init, control, seed))
+    }
+    
+    # Timestep loop
+    for (at in 2:control$nsteps) {
+      
+      ## Infection
+      if (!is.null(control[["infection.FUN"]])) {
+        dat <- do.call(control[["infection.FUN"]], list(dat, at, seed))
+      }
+      
+      
+      ## Recovery
+      if (!is.null(control[["recovery.FUN"]])) {
+        dat <- do.call(control[["recovery.FUN"]], list(dat, at, seed))
+      }
+      
+      
+      ## Departure Module
+      if (!is.null(control[["departures.FUN"]])) {
+        dat <- do.call(control[["departures.FUN"]], list(dat, at, seed))
+      }
+      
+      
+      ## Arrival Module
+      if (!is.null(control[["arrivals.FUN"]])) {
+        dat <- do.call(control[["arrivals.FUN"]], list(dat, at, seed))
+      }
+      
+      
+      ## Outputs
+      if (!is.null(control[["get_prev.FUN"]])) {
+        dat <- do.call(control[["get_prev.FUN"]], list(dat, at))
+      }
+      
+      
+      ## Track progress
+      EpiModel::verbose.icm(dat, type = "progress", s, at)
+    }
+    
+    # Set output
+    out <- saveout_seiqhrf(dat, s=1)
+    return(out)
+    
   }
-
-    # aggregate results collected from each thread
-    collected_times <- list()
-
-    # collect the times from sout then delete them
-    for (i in 1:length(sout)) {
-        collected_times[[paste0("sim", i)]] <- sout[[i]]$times$sim1
-        sout[[i]]$times <- NULL
-    }
-
-    # merge $epi structures
-    merged.out <- sout[[1]]
-    for (i in 2:length(sout)) {
-      merged.out <- merge.seiqhrf.icm(merged.out, sout[[i]], param.error = FALSE)
-    }
-    out <- merged.out
-
-    # add the collected timing data
-    out$times <- collected_times
-
-  } # end of parallel execution
+  
+  # aggregate results collected from each thread
+  collected_times <- list()
+  
+  # collect the times from sout then delete them
+  for (i in 1:length(sout)) {
+    collected_times[[paste0("sim", i)]] <- sout[[i]]$times$sim1
+    sout[[i]]$times <- NULL
+  }
+  
+  # merge $epi structures
+  merged.out <- sout[[1]]
+  for (i in 2:length(sout)) {
+    merged.out <- merge.seiqhrf.icm(merged.out, sout[[i]], param.error = FALSE)
+  }
+  out <- merged.out
+  
+  # add the collected timing data
+  out$times <- collected_times
   
   class(out) <- "seiqhrf"
-
+  
   return(out)
 }
--- R/globals.R
+++ R/globals.R
@@ -5,5 +5,6 @@ utils::globalVariables(c("time", "Date", "experiment", "compartment", "metric",
                          "CI.1", "CI.2", "qntCI.1", "qntCI.2", # plot_sirplus
                          "infTime", "expTime", "recovTime", "dischTime","hospTime", "quarTime", "fatTime",
                          "incubation_period", "quarantine_delay", "illness_duration", "illness_duration_hosp", 
-                         "hosp_los", "survival_time", "period_type", "duration" # plot_times
+                         "hosp_los", "survival_time", "period_type", "duration", # plot_times
+                         "s" # seiqhrf
                          ))
